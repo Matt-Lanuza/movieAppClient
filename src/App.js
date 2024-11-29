9eee58e917edb503759218104da98b135e8ce425
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-11-29 19:06:28 +0800

    add appnavbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,71 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
-  
+    const [user, setUser] = useState(() => {
+      const savedUser = localStorage.getItem('user');
+      return savedUser ? JSON.parse(savedUser) : { id: null };
+    });
+
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        console.error('No token found in localStorage');
+        return;
+      }
+
+      fetch('https://fitnessapi-lanuza.onrender.com/users/details', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("A user is logged in", data);
+
+          if (data !== undefined) {
+            setUser({
+              id: data.user.id,
+            });
+            localStorage.setItem('user', JSON.stringify({
+              id: data.user.id,
+            }));
+          } else {
+            setUser({
+              id: null,
+            });
+          }
+        })
+        .catch((err) => {
+          console.error('Error fetching user details:', err);
+          setUser({
+            id: null,
+          });
+        });
+    }, []);
+
+    function unsetUser() {
+      localStorage.clear();
+      setUser({ id: null });
+    }
+
+
+    return (
+    <UserProvider value={{ user, setUser, unsetUser }}>
+      <Router>
+        <AppNavbar />
+{/*        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>*/}
+      </Router>
+    </UserProvider>
+  );
 }
 
 export default App;
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function AppNavbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home" className="logo-name">CineGuru</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="navbar-collapse">Home</Nav.Link>
+            <Nav.Link href="#link" className="navbar-collapse">Login</Nav.Link>
+            <Nav.Link href="#link" className="navbar-collapse">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
