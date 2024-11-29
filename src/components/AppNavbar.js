429a9e5a146a3c4f5717a21735f0831a695784fb
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-11-29 19:34:32 +0800

    add login and register

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import UserContext from '../context/UserContext';
 
 export default function AppNavbar() {
+  const { user } = useContext(UserContext);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -11,9 +15,19 @@ export default function AppNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className="navbar-collapse">Home</Nav.Link>
-            <Nav.Link href="/login" className="navbar-collapse">Login</Nav.Link>
-            <Nav.Link href="/register" className="navbar-collapse">Register</Nav.Link>
+            <Nav.Link href="/" className="navbar-collapse">Home</Nav.Link>
+            
+            {user.id !== null ? (
+              <>
+                <Nav.Link href="/" className="navbar-collapse">My Workouts</Nav.Link>
+                <Nav.Link href="/logout" className="navbar-collapse">Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login" className="navbar-collapse">Login</Nav.Link>
+                <Nav.Link href="/register" className="navbar-collapse">Register</Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
